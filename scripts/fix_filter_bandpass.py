eea46cf2f33109a2e74bd5b6d605a3c313adddb0
Stuart Littlefair <s.littlefair@shef.ac.uk>
2017-06-02 16:04:02 +0100

    script to fix filter files

---
+++ scripts/fix_filter_bandpass.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+# Fix filter bandpass files from Vik Dhillon's ULTRACAM page into a
+# suitable format for pysynphot, and adjust for the f-ratio of the
+# beam.
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+import numpy as np
+import pysynphot as S
+
+
+def get_comments(fname):
+    comments = []
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('#'):
+                comments.append(line)
+    return comments
+
+
+def read_filter_file(fname):
+    try:
+        x, y = np.loadtxt(in_fname).T
+    except:
+        # format 2
+        # date
+        # time
+        # min wavelengt, max wavelength
+        # ?, ?
+        # ?, step
+        f = open(fname)
+        lines = f.readlines()
+        start, end = (float(x) for x in lines[2].split(','))
+        _, step = (float(x) for x in lines[4].split(','))
+        y = np.array([float(val.strip()) for val in lines[5:]])
+        x = start + step*np.arange(len(y))
+        # check we're close to the end value
+        assert np.allclose(x.max(), end)
+    return x, y
+
+
+def fix_filter(in_fname, out_fname, f_ratio):
+    x, y = np.loadtxt(in_fname).T
+    # x should be in AA, but is sometimes nm
+    if x.min() < 1000:
+        x *= 10
+    # should be in ascending order of x
+    indices = np.argsort(x)
+    x, y = x[indices], y[indices]
+
+    # f-ratio fix, assuming n=1.5
+    # ULTRACAM is f/2.3, HiPERCAM is f/2.47
+    theta = np.degrees(np.arctan(1/f_ratio/2))
+    n = 1.5
+    lambda_central = S.ArrayBandpass(x, y).pivot()
+    shift = 0.5*lambda_central*(np.sqrt(n**2-np.sin(theta)**2)/n - 1)
+    x -= shift
+
+    # if y is >1 we are in %
+    if y.max() > 1.5:
+        y /= 100
+
+    np.savetxt(out_fname, np.column_stack((x, y)))
+
+
+if __name__ == "__main__":
+    import sys
+    inst, in_fname, out_fname = sys.argv[1:]
+    assert inst in ('hcam', 'ucam')
+    out_fname = inst + '_' + out_fname + ".txt"
+    f_ratio = {'hcam': 2.47, 'ucam':2.3}[inst]
+
+    fix_filter(in_fname, out_fname, f_ratio)
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from __future__ import (print_function, absolute_import)
 from setuptools import setup
+import os
+import glob
 
 requirements = [
     'numpy',
     'astropy'
 ]
+
+# Treat everything in scripts except README.rst as a script to be installed
+scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
+           if os.path.basename(fname) != 'README.rst']
 
 setup(
     name="ucam_thruput",
@@ -13,6 +19,7 @@ setup(
     author_email="s.littlefair@shef.ac.uk",
     packages=['ucam_thruput'],
     package_data={'': ['data/*']},
+    scripts=scripts,
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False
