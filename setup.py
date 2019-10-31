beab48c17c36d0c9509029b7994687a16093c1af
Stuart Littlefair <s.littlefair@shef.ac.uk>
2019-10-31 15:55:27 +0000

    dont install scripts

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ requirements = [
     'astropy'
 ]
 
-# Treat everything in scripts except README.rst as a script to be installed
-scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
-           if os.path.basename(fname) != 'README.rst']
-
 setup(
     name="ucam_thruput",
     version="0.1",
@@ -19,7 +15,6 @@ setup(
     author_email="s.littlefair@shef.ac.uk",
     packages=['ucam_thruput'],
     package_data={'': ['data/*']},
-    scripts=scripts,
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False
