bdfa5322d7f5cb809b2e691976577d10e6c0291d
Stuart Littlefair <s.littlefair@shef.ac.uk>
2026-05-31 09:33:04 +0100

    remove pkg_resources

--- ucam_thruput/__init__.py
+++ ucam_thruput/__init__.py
@@ -1,25 +1,23 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from itertools import chain
 import os
 import datetime
-import pkg_resources
-import glob
+import importlib
 import shutil
 
 import numpy as np
 from astropy.table import Table
 
-INSTRUMENT_TABLE_NAME = 'ucam_thruput_tmg.fits'
-COMPONENT_TABLE_NAME = 'ucam_thruput_tmc.fits'
+INSTRUMENT_TABLE_NAME = "ucam_thruput_tmg.fits"
+COMPONENT_TABLE_NAME = "ucam_thruput_tmc.fits"
 # telescope areas in cm**2, corrected for obstructions
 TELESCOPE_AREAS = dict(
     ntt=90091.45,  # 3.58m, 1.16m obstruction
     vlt=518319.9,  # 8.2m, 1.116m obstruction with no baffle
     wht=127234.5,  # 4.2m, 1.2m obstruction
     gtc=730000.0,  # ?
-    tnt=37384.9,   # 2.3m clear aperture (2.4m, 1m obstruction)
+    tnt=37384.9,  # 2.3m clear aperture (2.4m, 1m obstruction)
 )
 
 
@@ -27,7 +25,7 @@ def _check_user_dir():
     """
     Check directories exist for saving apps/configs etc. Create if not.
     """
-    direc = os.path.expanduser('~/.ucam_thruput')
+    direc = os.path.expanduser("~/.ucam_thruput")
     if not os.path.exists(direc):
         os.mkdir(direc)
     return direc
@@ -38,9 +36,13 @@ def _check_tables():
     itable = os.path.join(user_dir, INSTRUMENT_TABLE_NAME)
     mtable = os.path.join(user_dir, COMPONENT_TABLE_NAME)
     if not os.path.exists(itable):
-        raise ValueError('Instrument graphtable {} is not installed'.format(INSTRUMENT_TABLE_NAME))
+        raise ValueError(
+            "Instrument graphtable {} is not installed".format(INSTRUMENT_TABLE_NAME)
+        )
     if not os.path.exists(mtable):
-        raise ValueError('Component table {} is not installed'.format(COMPONENT_TABLE_NAME))
+        raise ValueError(
+            "Component table {} is not installed".format(COMPONENT_TABLE_NAME)
+        )
     return itable, mtable
 
 
@@ -50,8 +52,8 @@ def list_keywords():
     """
     itable, _ = _check_tables()
     itable = Table.read(itable)
-    kws = set(itable['KEYWORD'])
-    kws.remove('default')
+    kws = set(itable["KEYWORD"])
+    kws.remove("default")
     return kws
 
 
@@ -67,9 +69,7 @@ def getref(telescope):
     assert telescope in TELESCOPE_AREAS
     itable, mtable = _check_tables()
     ref_dict = dict(
-        area=TELESCOPE_AREAS[telescope],
-        graphtable=itable,
-        comptable=mtable
+        area=TELESCOPE_AREAS[telescope], graphtable=itable, comptable=mtable
     )
     return ref_dict
 
@@ -86,14 +86,12 @@ def _install_table_files():
     itable = _make_instrument_reference_table()
     mtable = _make_component_table(itable)
     user_dir = _check_user_dir()
-    itable.write(os.path.join(user_dir, INSTRUMENT_TABLE_NAME),
-                 overwrite=True)
-    mtable.write(os.path.join(user_dir, COMPONENT_TABLE_NAME),
-                 overwrite=True)
+    itable.write(os.path.join(user_dir, INSTRUMENT_TABLE_NAME), overwrite=True)
+    mtable.write(os.path.join(user_dir, COMPONENT_TABLE_NAME), overwrite=True)
 
 
 def _install_throughput_files():
-    pysyn_cdbs = os.getenv('PYSYN_CDBS')
+    pysyn_cdbs = os.getenv("PYSYN_CDBS")
     if pysyn_cdbs is None:
         err_msg = """
             PYSYN_CDBS environment variable is not set.
@@ -106,7 +104,7 @@ def _install_throughput_files():
             """
         raise ValueError(err_msg)
 
-    pysyn_cdbs = os.path.join(pysyn_cdbs, 'comp/nonhst')
+    pysyn_cdbs = os.path.join(pysyn_cdbs, "comp/nonhst")
     if not os.path.exists(pysyn_cdbs):
         err_msg = """
             Directory $PYSYN_CDBS/comp/nonhst does not exist.
@@ -119,8 +117,8 @@ def _install_throughput_files():
             """
         raise ValueError(err_msg)
 
-    resource_dir = pkg_resources.resource_filename('ucam_thruput', 'data')
-    for filename in glob.glob(os.path.join(resource_dir, '*.txt')):
+    resource_dir = importlib.resources.files("ucam_thruput") / "data"
+    for filename in resource_dir.glob("*.txt"):
         shutil.copy(filename, pysyn_cdbs)
 
 
@@ -129,32 +127,53 @@ def _make_instrument_reference_table():
     from .uspec import Uspec
     from .common import Common
     from .hcam import Hcam
+
     c = Common()
     u = Ucam()
     us = Uspec()
     hc = Hcam()
-    rows = list(chain(*(c.instrument_table_rows,
-                        u.instrument_table_rows,
-                        hc.instrument_table_rows,
-                        us.instrument_table_rows)))
+    rows = list(
+        chain(
+            *(
+                c.instrument_table_rows,
+                u.instrument_table_rows,
+                hc.instrument_table_rows,
+                us.instrument_table_rows,
+            )
+        )
+    )
     rows = sorted(rows, key=lambda x: x[2])
 
-    table = Table(names=['COMPNAME', 'KEYWORD', 'INNODE', 'OUTNODE', 'THCOMPNAME', 'COMMENT'],
-                  dtype=(np.dtype((str, 18)), np.dtype((str, 20)), 'int32', 'int32',
-                         np.dtype((str, 20)), np.dtype((str, 68))))
+    table = Table(
+        names=["COMPNAME", "KEYWORD", "INNODE", "OUTNODE", "THCOMPNAME", "COMMENT"],
+        dtype=(
+            np.dtype((str, 18)),
+            np.dtype((str, 20)),
+            "int32",
+            "int32",
+            np.dtype((str, 20)),
+            np.dtype((str, 68)),
+        ),
+    )
     for row in rows:
         table.add_row(row)
     return table
 
 
 def _make_component_table(itable):
-    table = Table(names=['TIME', 'COMPNAME', 'FILENAME', 'COMMENT'],
-                  dtype=(np.dtype((str, 26)), np.dtype((str, 18)),
-                         np.dtype((str, 50)), np.dtype((str, 68))))
-    components = set(itable['COMPNAME'])
-    filenames = ['crnonhstcomp$' + component + '.txt' for component in components]
+    table = Table(
+        names=["TIME", "COMPNAME", "FILENAME", "COMMENT"],
+        dtype=(
+            np.dtype((str, 26)),
+            np.dtype((str, 18)),
+            np.dtype((str, 50)),
+            np.dtype((str, 68)),
+        ),
+    )
+    components = set(itable["COMPNAME"])
+    filenames = ["crnonhstcomp$" + component + ".txt" for component in components]
     now = datetime.datetime.now()
-    time_string = now.strftime('%b %d %Y %H:%M:%S').lower()
+    time_string = now.strftime("%b %d %Y %H:%M:%S").lower()
     for component, filename in zip(components, filenames):
-        table.add_row((time_string, component, filename, 'ultracam group throughput'))
+        table.add_row((time_string, component, filename, "ultracam group throughput"))
     return table
