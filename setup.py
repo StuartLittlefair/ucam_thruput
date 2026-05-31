6c5722925d030b571d2f9e044512626f3901f85f
Stuart Littlefair <s.littlefair@shef.ac.uk>
2026-05-31 09:33:35 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
-from __future__ import (print_function, absolute_import)
+from __future__ import print_function, absolute_import
 from setuptools import setup
 import os
 import glob
 
-requirements = [
-    'numpy',
-    'astropy'
-]
+requirements = ["numpy", "astropy" "synphot", "stsynphot"]
 
 setup(
     name="ucam_thruput",
     version="0.1",
     description="Instrument Simulations for HiPERCAM, ULTRACAM, ULTRASPEC",
     author_email="s.littlefair@shef.ac.uk",
-    packages=['ucam_thruput'],
-    package_data={'': ['data/*']},
+    packages=["ucam_thruput"],
+    package_data={"": ["data/*"]},
     include_package_data=True,
     install_requires=requirements,
-    zip_safe=False
+    zip_safe=False,
 )
